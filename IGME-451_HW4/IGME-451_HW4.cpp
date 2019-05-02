38635aa8286ae09bb8eff0c528610bd8657c4ce8
bxs9775 <bxs9775@rit.edu>
2019-05-02 17:15:31 -0400

    Sleeping Barbers sample/starter code from ??? chapter 2 * needs to be fixed/adjusted for C++ lock methods...

--- IGME-451_HW4/IGME-451_HW4.cpp
+++ IGME-451_HW4/IGME-451_HW4.cpp
@@ -4,22 +4,60 @@
 #include "pch.h"
 #include <iostream>
 
+// Stores the number of chairs, customers, and barbers to use in the process...
 int numberOfChairs;
 int numberOfCustomers;
 int numberOfBarbers;
 
-void Customer(void) {
+// Thread management variables
+typedef int semaphore;
+semaphore customers;
+semaphore barbers;
+semaphore chairs;
+int waiting = 0;
 
+//placeholder method for lock
+void down(semaphore mutex) {
+}
+
+//placeholder method for lock
+void up(semaphore mutex) {
+}
+
+void get_haircut() {}
+
+void cut_hair() {}
+
+void Customer(void) {
+	down(chairs);
+	if (waiting < numberOfChairs) {
+		waiting = waiting + 1;
+		up(customers);
+		up(chairs);
+		down(barbers);
+		get_haircut();
+	}
+	else {
+		up(chairs);
+	}
 }
 
 void Barber(void) {
-
+	while (true) {
+		down(customers);
+		down(chairs);
+		waiting = waiting - 1;
+		up(barbers);
+		up(chairs);
+		cut_hair();
+	}
 }
 
 
 int main()
 {
-    std::cout << "How many chairs -> "; 
+    /*
+	std::cout << "How many chairs -> "; 
 	std::cin >> numberOfChairs;
 	std::cout << "How many customers -> ";
 	std::cin >> numberOfCustomers;
@@ -38,4 +76,11 @@ int main()
 		std::cout << "There are no barbers on staff. End program.\n";
 		return;
 	}
+	*/
+
+	//Using set values first to get things working...
+	numberOfChairs = 5;
+	numberOfBarbers = 1;
+	numberOfCustomers = 5;
+
 }
