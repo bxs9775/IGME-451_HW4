108ca8cefd49f9e0dd71606fd8da9fa11708a34d
bxs9775 <bxs9775@rit.edu>
2019-05-05 10:16:16 -0400

    Clean up commented code, and added minimal documentation

--- IGME-451_HW4/IGME-451_HW4.cpp
+++ IGME-451_HW4/IGME-451_HW4.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <mutex>
 
+// Macro for the process of checking if their are customers left and releasing mutexes
 #define CHECK_CUSTOMERS_LEFT \
 if (customersLeft <= 0) {\
 	std::call_once(barbersEnded,[](){customerReady.notify_all();});\
@@ -18,17 +19,22 @@ int numberOfChairs;
 int numberOfCustomers;
 int numberOfBarbers;
 
-// Thread management variables
-std::mutex lock;
+// *** Mutex variables ***
+// Mutex for controlling a barber's access to information on the customers
 std::mutex cMutex;
+// Mutex for controlling a customer's access to information on the barbers
 std::mutex bMutex;
+// Mutex protecting waiting variable which keeps track of chair occupency
 std::mutex chairMutex;
+// Mutex protecting print operations to std::cout.
 std::mutex printMutex;
 
+// Variables for keeping track where different threads are.
 int waiting = 0;
 int customersLeft;
-
 int barbersWaiting = 0;
+
+// Other mutex/thread related variables
 std::condition_variable customerReady;
 std::condition_variable barberReady;
 std::once_flag barbersEnded;
@@ -42,9 +48,6 @@ public:
 	}
 
 	Customer(int value) {
-		printMutex.lock();
-		std::cout << "C #" << value << " - created." << std::endl;
-		printMutex.unlock();
 		id = value;
 	}
 
@@ -52,41 +55,42 @@ public:
 
 	}
 
+	// Prints out the fact that the customer got a hair cut.
 	void get_haircut() {
 		printMutex.lock();
 		std::cout << "Customer #" << id << " got their hair cut." << std::endl;
 		printMutex.unlock();
 	}
 
+	// Function for running the customer process.
 	void run() {
-		/*
-		printMutex.lock();
-		std::cout << "C #" << id << " - started." << std::endl;
-		printMutex.unlock();
-		*/
 		chairMutex.lock();
 		if (waiting < numberOfChairs) {
+			// if there are chairs sit down and wait for barber
 			std::unique_lock<std::mutex> l(bMutex);
+
+			// Lets the barbers know its ready
 			waiting = waiting + 1;
 			customerReady.notify_one();
+
 			chairMutex.unlock();
+
+			//Waits for a barber to be ready
 			barberReady.wait(l, [this]() { return barbersWaiting > 0; });
 			--barbersWaiting;
-			//std::cout << "C #" << id << " - customers waiting = " << waiting << std::endl;
+
 			get_haircut();
-			//std::cout << "C #" << id << " - waiting = " << waiting << std::endl;
+
 			l.unlock();
-			/*
-			printMutex.lock();
-			std::cout << "C #" << id << " - stopped." << std::endl;
-			printMutex.unlock();
-			*/
 		}
 		else {
+			// if the chairs are full leave
 			--customersLeft;
+
 			printMutex.lock();
-			std::cout << " #" << id << " - stopped." << std::endl;
+			std::cout << "Customer #" << id << " couldn't find an empty chair and left." << std::endl;
 			printMutex.unlock();
+
 			chairMutex.unlock();
 		}
 	}
@@ -105,39 +109,42 @@ public:
 		id = value;
 	}
 
+	// Prints out the fact the barber is cutting hair
 	void cut_hair() {
 		printMutex.lock();
 		std::cout << "Barber #" << id << " snips away with their shears." << std::endl;
 		printMutex.unlock();
 	}
 	
+	// Runs the barber's loop that checks if customers are ready and cuts their hair.
 	void run(){
 		while (true) {
 			//Reduces but does not elimate the risk that the barber continues on with a new customer 
 			//before the current customer registers that their hair is cut
 			bMutex.lock();
 			bMutex.unlock();
-			/*
-			printMutex.lock();
-			std::cout << "B #" << id << " - started." << std::endl;
-			printMutex.unlock();
-			*/
+
+
 			std::unique_lock<std::mutex> l(cMutex);
-			//std::cout << "B #" << id << " - waiting = " << waiting << std::endl;
+			// Waits until a at least one customer is ready or all the customers have left...
 			customerReady.wait(l, [this]() { return waiting > 0 | customersLeft <= 0; });
-			//std::cout << "B #" << id << " - customers waiting = " << waiting << std::endl;
 			chairMutex.lock();
 			CHECK_CUSTOMERS_LEFT
+			// Lets customer know its ready
 			++barbersWaiting;
 			barberReady.notify_one();
+
+			//Updates customer info
 			waiting = waiting - 1;
 			--customersLeft;
 			chairMutex.unlock();
+
 			cut_hair();
-			//--barbersWaiting;
+
 			chairMutex.lock();
 			CHECK_CUSTOMERS_LEFT
 			chairMutex.unlock();
+
 			l.unlock();
 		}
 	}
@@ -149,6 +156,7 @@ int main()
 {
 	barbersWaiting = 0;
 
+	// Gets the number of chairs, customers, and barbers from user.
 	std::cout << "How many chairs -> "; 
 	std::cin >> numberOfChairs;
 	std::cout << "How many customers -> ";
@@ -156,6 +164,7 @@ int main()
 	std::cout << "How many barbers -> ";
 	std::cin >> numberOfBarbers;
 
+	// Checks if values are > 0
 	if (!numberOfCustomers) {
 		std::cout << "There are no customers. End program.\n";
 		return -1;
@@ -171,6 +180,7 @@ int main()
 
 	customersLeft = numberOfCustomers;
 
+	// Sets up cThreads (customer threads) and bThreads (barber threads)
 	std::thread* cThreads = new std::thread[numberOfCustomers];
 	std::thread* bThreads = new std::thread[numberOfBarbers];
 
@@ -181,6 +191,7 @@ int main()
 		cThreads[i] = std::thread(&Customer::run,Customer(i));
 	}
 
+	// calls join on threads to sycronise the program at the end...
 	for (int i = 0; i < numberOfCustomers; i++) {
 		cThreads[i].join();
 	}
@@ -188,6 +199,7 @@ int main()
 		bThreads[i].join();
 	}
 
+	// Cleans up arrays.
 	delete[] cThreads;
 	delete[] bThreads;
 }
